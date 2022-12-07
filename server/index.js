4f3aeac8b80bf2862d18ad3ca8186ac4efebe09c
dullfarhan <dullfarhan@gmail.com>
2022-12-07 21:23:41 +0500

    fix

--- server/index.js
+++ server/index.js
@@ -14,10 +14,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb://localhost:27017/test';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('db connected');
+})
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
